715d4e320fce56a08e44201772dd1a168998ec1d
syairaarif <putrisyafira4444@gmail.com>
2024-04-02 11:00:25 +0700

    mendeklarasikan variabel

---
+++ project4/project4.cpp
@@ -0,0 +1,23 @@
+
+
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat
+{
+    string desa;
+    string kota;
+};
+
+struct mahasiswa
+{
+    string nim;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
+int main()
+{
+    
+}
