db88e0dceac9d0286d0ca839a374178b8154a4af
syairaarif <putrisyafira4444@gmail.com>
2024-04-02 10:18:05 +0700

    mendeklarasikan variabel yang akan digunakan

---
+++ StrukturKelas/StrukturKelas.cpp
@@ -0,0 +1,25 @@
+
+
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat
+{
+    string desa;
+    string kota;
+};
+
+struct mahasiswa
+{
+    string nim;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
+int main()
+{
+    
+}
+
+
