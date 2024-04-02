1c8f57ca5b531b019e77afbf1c8729e4dc625c64
syairaarif <putrisyafira4444@gmail.com>
2024-04-02 10:56:38 +0700

    membuat ccout dan cin dalam tabel mahasiswa

--- Project3/Project3.cpp
+++ Project3/Project3.cpp
@@ -11,12 +11,29 @@ struct DetailAlamat
 struct mahasiswa
 {
     string nim;
-    string nama[25];
+    string nama;
     DetailAlamat alamat;
     int umur;
 };
+
+
 int main()
 {
+    mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    getline(cin, mhs.nim);
+    cout << "Nama Mahasiswa : ";
+    getline (cin , mhs.nama);
+
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota : ";
+    cin >> mhs.alamat.kota;
+
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+
     
 }
 
